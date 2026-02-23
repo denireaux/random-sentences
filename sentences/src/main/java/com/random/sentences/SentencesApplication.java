4368b5b24649486d21d6a8d7716ac2991303a22d
denireaux <dangelo.l.deniro@gmail.com>
2026-02-23 12:26:15 -0700

    Added spring service

---
+++ sentences/src/main/java/com/random/sentences/SentencesApplication.java
@@ -0,0 +1,13 @@
+package com.random.sentences;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentencesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentencesApplication.class, args);
+	}
+
+}
