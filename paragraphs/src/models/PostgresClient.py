1cfd197a705e8b0e5e725d4aabcf3a02764a68c8
denireaux <dangelo.l.deniro@gmail.com>
2026-03-16 23:31:42 -0600

    rows

--- paragraphs/src/models/PostgresClient.py
+++ paragraphs/src/models/PostgresClient.py
@@ -37,3 +37,63 @@ class PostgresClient():
         finally:
             if conn:
                 conn.close()
+
+    def pop_last_five_sentences(self):
+        conn = None
+        try:
+            conn = psycopg2.connect(
+                host=self.host,
+                port=self.port,
+                dbname=self.dbname,
+                user=self.user,
+                password=self.password
+            )
+
+            cursor = conn.cursor()
+            are_five_sentences_available = self._check_for_five_sentences(cursor)
+
+            if are_five_sentences_available is False:
+                cursor.close()
+                return []
+
+            query = """
+                DELETE FROM generated_sentences
+                WHERE id IN (
+                    SELECT id
+                    FROM generated_sentences
+                    ORDER BY created_at DESC
+                    LIMIT 5
+                )
+                RETURNING sentence_text, generated_by, created_at;
+            """
+
+            cursor.execute(query)
+            rows = cursor.fetchall()
+
+            conn.commit()
+            cursor.close()
+
+            print("\nCollected 5 sentences:")
+            for r in rows:
+                print(f"{r[2]} | {r[1]} | {r[0]}")
+
+            return rows
+
+        except psycopg2.Error as e:
+            print(f"Postgres Error: {e}")
+            return []
+
+        finally:
+            if conn:
+                conn.close()
+
+
+    def _check_for_five_sentences(self, cursor):
+        cursor.execute("SELECT COUNT(*) FROM generated_sentences")
+        count = cursor.fetchone()[0]
+
+        if count < 5:
+            return False
+
+        return True
+        
